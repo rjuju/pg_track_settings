9a856380862599dd0b8dba49b30940a7ef6a6237
Julien Rouhaud <julien.rouhaud@free.fr>
2022-04-24 09:54:45 +0800

    Start working on 2.1.0

---
+++ pg_track_settings--2.0.1--2.1.0.sql
@@ -0,0 +1,9 @@
+-- This program is open source, licensed under the PostgreSQL License.
+-- For license terms, see the LICENSE file.
+--
+-- Copyright (C) 2015-2022: Julien Rouhaud
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION pg_track_settings" to load this file. \quit
+
+SET LOCAL client_encoding = 'UTF8';
